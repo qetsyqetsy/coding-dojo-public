0dab6c8fb12088773f8b8be71aef397dd3a1a95d
qetsyqetsy <qetsyqetsy@gmail.com>
2024-05-12 17:49:23 -0400

    [Tarea] Poniendo todo junto

--- PRACTICA/apoyalo/src/components/PersonCard.js
+++ PRACTICA/apoyalo/src/components/PersonCard.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 const PersonCard = ({ firstName, lastName, age, hairColor}) => {
+    const [personAge, setPersonAge] = useState(age);
+
+    const handleBirthday = () => {
+        setPersonAge(personAge + 1);
+    }
     return (
         <div>
             <h2>{firstName} {lastName}</h2>
-            <p>Age: {age}</p>
+            <p>Age: {personAge}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={handleBirthday}>Celebrate Birthday</button>
         </div>
     );
 };
 
 export default PersonCard;
+
